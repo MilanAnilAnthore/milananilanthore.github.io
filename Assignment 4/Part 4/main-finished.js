e4125ed0df3debbdc83dbef4659c26a9cf5f4832
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2024-03-21 15:45:48 -0400

    object
    made an evil circle object

--- Assignment 4/Part 4/main-finished.js
+++ Assignment 4/Part 4/main-finished.js
@@ -184,6 +184,8 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const darkBall = new EvilCircle(random(0, width), random(0, height));
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
