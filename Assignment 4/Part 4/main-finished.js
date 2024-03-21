fef57d42feb5e0b03872c5ef2f524b9c75125e2f
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2024-03-21 15:20:30 -0400

    EventListener is added

--- Assignment 4/Part 4/main-finished.js
+++ Assignment 4/Part 4/main-finished.js
@@ -101,6 +101,22 @@ class EvilCircle extends Shape {
   }
 }
 
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
 
 
 const balls = [];
