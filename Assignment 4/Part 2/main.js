ef97471e166357140f2da4df0d1793cdbcfeb532
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2024-03-22 01:14:02 -0400

    image filenames
    an array of image filenames is created

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -11,6 +11,8 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
@@ -21,3 +23,5 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+
+
